8caa0b2d456de606f6f5cceda802007cf2070716
wangcx <591554596@qq.com>
2018-03-16 19:35:08 +0800

    test1

---
+++ app/lib/AddOrderTask.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangcx
+ * Date: 18-3-16
+ * Time: 下午7:21
+ */
+
+namespace app\lib;
+
+
+class AddOrderTask
+{
+    protected  $wid;
+    public function __construct($wid)
+    {
+        $this->wid = $wid;
+    }
+
+
+    public function setTaskName(){
+
+    }
+
+    public function setTaskCycle(){
+
+    }
+}
