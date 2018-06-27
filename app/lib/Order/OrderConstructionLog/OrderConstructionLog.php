b8e3237c470bdb4c6757057c37dd5142a9b23725
wangcx <591554596@qq.com>
2018-06-27 17:50:34 +0800

    工地日志

---
+++ app/lib/Order/OrderConstructionLog/OrderConstructionLog.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018\6\27 0027
+ * Time: 16:19
+ */
+
+namespace app\lib\Order\OrderConstructionLog;
+
+
+class OrderConstructionLog
+{
+
+    public function setOrderId(){
+
+    }
+
+    public function setOrderConstructionLogId(){
+
+    }
+
+    /**
+     * 当看了工地日志就写入已读
+     * 阅读写入
+     */
+    public function readWriting(){
+
+    }
+
+    /**
+     * 获取顾客未读
+     */
+    public function getCustomerNoRead(){
+
+    }
+
+    /**
+     * 获取顾客未读总条数
+     */
+    public function getCustomerNoReadNum(){
+
+    }
+
+    /**
+     * 获取指定用户未读
+     */
+    public function getUserNoRead(){
+
+    }
+    /**
+     * 获取指定用户未读
+     */
+    public function getUserNoReadNum(){
+
+    }
+
+}
